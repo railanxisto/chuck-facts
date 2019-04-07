c7735ae1814839fe5dd1bdcd8bfd9014bac35745
Railan Xisto <railan.xisto@gmail.com>
2019-04-07 13:17:32 -0300

    Add some espresso tests

--- app/src/androidTest/java/br/com/railanxisto/chuckfacts/MockService.kt
+++ app/src/androidTest/java/br/com/railanxisto/chuckfacts/MockService.kt
@@ -8,7 +8,10 @@ import org.mockito.Mockito
 
 class MockService(private val chuckFactsService: ChuckFactsService) {
 
-    val query = "food"
+    val QUERY = "food"
+    val QUERY_EMPTY = "QUERY_EMPTY"
+    val QUERY_ERROR = "QUERY_ERROR"
+
     val queryTermForError = ""
     val listOfCategories = listOf("food", "dev")
     val listOfFacts = listOf(
@@ -17,23 +20,26 @@ class MockService(private val chuckFactsService: ChuckFactsService) {
     )
 
     init {
-        mockResponseSucess()
-
-        // mockResponseOfErrorInQuery()
-
+        mockResponseOfFacts()
         mockResponseOfGetCategories()
     }
 
-    private fun mockResponseSucess() {
+    private fun mockResponseOfFacts() {
         val response = FactsResponse(2, listOfFacts)
+        val responseEmpty = FactsResponse(2, listOf())
 
-        Mockito.`when`(chuckFactsService.getFacts(query))
+        Mockito.`when`(chuckFactsService.getFacts(QUERY))
+            .thenReturn(Single.just(response))
+
+        Mockito.`when`(chuckFactsService.getFacts(QUERY_EMPTY))
+            .thenReturn(Single.just(responseEmpty))
+
+        Mockito.`when`(chuckFactsService.getFacts(QUERY_ERROR))
             .thenReturn(Single.just(response))
     }
-
     /* private fun mockResponseOfErrorInQuery() {
-        val expectedResponseBody = ResponseBody.create(MediaType.get("text/plain"), "Internal server error")
-        //val expectedResponse2 = Response.error<QueryFactsResponse>(500, expectedResponseBody)
+        val expectedResponseBody = ResponseBody.create(MediaType.get("text/plain"), "Internal server QUERY_ERROR")
+        //val expectedResponse2 = Response.QUERY_ERROR<QueryFactsResponse>(500, expectedResponseBody)
 
         Mockito.`when`(se.queryFacts(queryTermForError))
             .thenReturn(Single.just(expectedResponse2))
--- app/src/androidTest/java/br/com/railanxisto/chuckfacts/presentation/FactsActivityTest.kt
+++ app/src/androidTest/java/br/com/railanxisto/chuckfacts/presentation/FactsActivityTest.kt
@@ -2,14 +2,19 @@ package br.com.railanxisto.chuckfacts.presentation
 
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import br.com.railanxisto.chuckfacts.MockService
 import br.com.railanxisto.chuckfacts.R
+import br.com.railanxisto.chuckfacts.config.ToastMatcher
 import br.com.railanxisto.chuckfacts.data.remote.ChuckFactsService
 import br.com.railanxisto.chuckfacts.presentation.facts.FactsActivity
 import org.junit.Assert
@@ -17,20 +22,27 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.koin.test.AutoCloseKoinTest
+import org.koin.test.KoinTest
 import org.koin.test.inject
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
-class FactsActivityTest : AutoCloseKoinTest() {
+class FactsActivityTest : KoinTest {
     val service: ChuckFactsService by inject()
+    private lateinit var mockService: MockService
 
     @get:Rule
     var activity = ActivityScenarioRule(FactsActivity::class.java)
 
     @Before
     fun setUp() {
-        MockService(service)
+        mockService = MockService(service)
+    }
+
+    @Test
+    fun checkIfFactsActivityIsShown() {
+        Espresso.onView(ViewMatchers.withId(R.id.action_search))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 
     @Test
@@ -50,16 +62,45 @@ class FactsActivityTest : AutoCloseKoinTest() {
     }
 
     @Test
-    fun checkMock() {
+    fun checkIfItGoesBackToFactsActivityWhenPressBackButton() {
+        Espresso.onView(ViewMatchers.withId(R.id.action_search)).perform(ViewActions.click())
+        Espresso.pressBack()
+        checkIfFactsActivityIsShown()
+    }
+
+    @Test
+    fun checkIfItShowsCorrectNumberOfResultsOnFactsActivity() {
         Espresso.onView(ViewMatchers.withId(R.id.action_search)).perform(ViewActions.click())
         Espresso.onView(ViewMatchers.withId(R.id.searchEditText)).perform(
-            ViewActions.typeText("food"),
+            ViewActions.typeText(mockService.QUERY),
             ViewActions.pressImeActionButton()
         )
         Espresso.onView(ViewMatchers.withId(R.id.factsRecyclerView)).check { view, _ ->
             Assert.assertEquals(RecyclerView::class.java, view::class.java)
             val recyclerView = view as RecyclerView
-            Assert.assertTrue(recyclerView.adapter?.itemCount == 2)
+            Assert.assertTrue(recyclerView.adapter?.itemCount == mockService.listOfFacts.size)
         }
+    }
+
+    @Test
+    fun checkIfItShowsSuggestions() {
+        Espresso.onView(ViewMatchers.withId(R.id.action_search)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.suggestionsRecyclerView)).check { view, _ ->
+            Assert.assertEquals(RecyclerView::class.java, view::class.java)
+            val recyclerView = view as RecyclerView
+            Assert.assertNotNull(recyclerView.adapter)
+            Assert.assertTrue(recyclerView.adapter?.itemCount != 0)
+        }
+    }
+
+    @Test
+    fun checkIfItShowsToastWhenNoResultsForTerm() {
+        Espresso.onView(ViewMatchers.withId(R.id.action_search)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.searchEditText)).perform(
+            ViewActions.typeText(mockService.QUERY_EMPTY),
+            ViewActions.pressImeActionButton()
+        )
+        Thread.sleep(500)
+        onView(withText(R.string.no_results)).inRoot(ToastMatcher()).check(matches(isDisplayed()))
     }
 }
